376ed4da4c7638f8c8525ba62d9d3b4b95d40bc1
000Vero <mattiapasquadibisceglia@gmail.com>
2025-04-02 21:38:02 +0200

    Add files

---
+++ tow/game.js
@@ -0,0 +1,58 @@
+/* ===== GAME VARIABLES ===== */
+
+const lBar = document.getElementById("lBar");
+const rBar = document.getElementById("rBar");
+const announcer = document.getElementById("announcer");
+
+const restartButton = '<button class="button is-primary" onclick="restart()" style="position: relative; top: 50%; transform: translateY(-50%);">Restart</button>';
+
+var position = 50;
+var started = false;
+var timeToStart = 3000;
+
+var interval = setInterval(startGame, 1000);
+
+/* ===== GAME FUNCTIONS AND HANDLING ===== */
+
+function startGame() {
+    timeToStart -= 1000;
+
+    if (timeToStart == 0) {
+        started = true;
+        announcer.innerText = "Go!";
+        clearInterval(interval);
+    } else {
+        announcer.innerText = timeToStart / 1000;
+    }
+}
+
+function tug(amount) {
+    if (!started) return;
+    position += amount;
+    lBar.style.width = position.toString() + "%";
+    rBar.style.width = (100 - position).toString() + "%";
+
+    if (position == 100) {
+        lBar.style.borderTopRightRadius = lBar.style.borderBottomRightRadius = "var(--bulma-radius-rounded)";
+        announcer.innerText = "Blue wins! ";
+        announcer.innerHTML += restartButton;
+        started = false;
+    } else if (position == 0) {
+        rBar.style.borderTopLeftRadius = rBar.style.borderBottomLeftRadius = "var(--bulma-radius-rounded)";
+        announcer.innerText = "Red wins! ";
+        announcer.innerHTML += restartButton;
+        started = false;
+    }
+}
+
+function restart() {
+    clearInterval(interval);
+    started = false;
+    position = 50;
+    lBar.style.width = "50%";
+    rBar.style.width = "50%";
+    lBar.style.borderTopRightRadius = lBar.style.borderBottomRightRadius = rBar.style.borderTopLeftRadius = rBar.style.borderBottomLeftRadius = "0px";
+    timeToStart = 3000;
+    announcer.innerText = timeToStart / 1000;
+    interval = setInterval(startGame, 1000);
+}
